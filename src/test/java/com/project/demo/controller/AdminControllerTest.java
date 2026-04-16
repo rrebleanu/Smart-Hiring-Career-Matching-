7d3390965c4ae1c28bdc3a2d0551408da992a7bc
Rebleanu Robert <rrebleanu@gmail.com>
2026-04-16 16:57:20 +0300

    adaugare folder imagini

---
+++ src/test/java/com/project/demo/controller/AdminControllerTest.java
@@ -0,0 +1,19 @@
+package com.project.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@WebMvcTest(AdminController.class)
+class AdminControllerTest {
+
+    @Test
+    void addNewAdmin() {
+
+    }
+
+    @Test
+    void getAllAdmins() {
+    }
+}
+
